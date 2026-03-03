d707b3d79e8ae51ddbe90761ab5e95a54d4a5e06
JinYong833 <jjystudy1@gmail.com>
2026-03-03 12:03:56 +0900

    first commit

---
+++ src/main/java/com/sogonsogon/gonggomoonbackofficeapi/GonggomoonBackofficeApiApplication.java
@@ -0,0 +1,13 @@
+package com.sogonsogon.gonggomoonbackofficeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonggomoonBackofficeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GonggomoonBackofficeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sogonsogon/gonggomoonbackofficeapi/GonggomoonBackofficeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogonsogon.gonggomoonbackofficeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonggomoonBackofficeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
