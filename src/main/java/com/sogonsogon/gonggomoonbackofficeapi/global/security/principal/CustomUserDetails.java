0025cdf988c4c60f86c7a3866b44445864e3e430
JinYong833 <jjystudy1@gmail.com>
2026-03-07 10:08:33 +0900

    feat(user): 로그인 및 @authenticationprincipal을 사용하기 위한 공통 작업
    - 글로벌 익셉션 핸들러 및 베이스 익셉션 동기화 작업 이후 에러 수정 필요함
    - 유저 엔티티 동기화 이후 토큰에 넣는 정보가 바뀌면 해당 로직 리팩토링 해야함

--- src/main/java/com/sogonsogon/gonggomoonbackofficeapi/global/security/principal/CustomUserDetails.java
+++ src/main/java/com/sogonsogon/gonggomoonbackofficeapi/global/security/principal/CustomUserDetails.java
@@ -1,6 +1,7 @@
 package com.sogonsogon.gonggomoonbackofficeapi.global.security.principal;
 
 import com.sogonsogon.gonggomoonbackofficeapi.domain.user.entity.User;
+import com.sogonsogon.gonggomoonbackofficeapi.domain.user.entity.UserRole;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -16,6 +17,17 @@ public class CustomUserDetails implements UserDetails {
 
     public CustomUserDetails(User user) {
         this.user = user;
+    }
+
+    public static CustomUserDetails ofToken(Long id, Collection<? extends GrantedAuthority> authorities) {
+        // 토큰에 담긴 권한 문자열(ROLE_ADMIN 등)을 다시 UserRole Enum으로 역변환
+        String authority = authorities.iterator().next().getAuthority();
+        UserRole role = UserRole.fromAuthority(authority);
+
+        // 빌더가 생성자에 붙어 있으므로, 필요한 필드만 채워서 가짜 유저를 만듭니다.
+        User tempUser = User.createAuthorityUser(id, role);
+
+        return new CustomUserDetails(tempUser);
     }
 
     @Override
