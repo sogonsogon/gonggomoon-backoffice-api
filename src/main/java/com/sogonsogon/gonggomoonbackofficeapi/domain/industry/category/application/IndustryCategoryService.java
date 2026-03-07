988e851f19b396e7db8484bccb68330e698160e3
JinYong833 <jjystudy1@gmail.com>
2026-03-07 18:46:56 +0900

    [Feat] 산업 카테고리 목록 조회 (#4)
    * feat(IndustryCategory): 카테고리 목록 조회
    
    - 생성일 기준 오름차순으로 정렬해서 반환
    
    * refactor(securityconfig): 검증 취소
    
    - 모든 요청을 permitAll로 바꿈
    
    * refactor(securityconfig): 검증 시작

--- src/main/java/com/sogonsogon/gonggomoonbackofficeapi/domain/industry/category/application/IndustryCategoryService.java
+++ src/main/java/com/sogonsogon/gonggomoonbackofficeapi/domain/industry/category/application/IndustryCategoryService.java
@@ -2,10 +2,14 @@ package com.sogonsogon.gonggomoonbackofficeapi.domain.industry.category.applicat
 
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.category.dto.request.CreateIndustryCategoryRequest;
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.category.dto.request.UpdateIndustryCategoryRequest;
+import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.category.dto.response.IndustryCategoryResponse;
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.category.entity.IndustryCategory;
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.category.infrastructure.IndustryCategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class IndustryCategoryService {
@@ -39,5 +43,26 @@ public class IndustryCategoryService {
                 .orElseThrow(IllegalArgumentException::new);
 
         industryCategory.update(request.industryCategoryName(), userId);
+    }
+
+    /**
+     * 카테고리 목록 조회
+     * content로 감싸는게 dto에 있어야 할 듯?
+     * List로 만드는거 까지?
+     * 생성일 기준 오름차순 정렬
+     */
+    @Transactional(readOnly = true)
+    public Map<String, List<IndustryCategoryResponse>> getIndustryCategorise() {
+
+        List<IndustryCategory> categories = industryCategoryRepository.findAllByOrderByCreatedAtAsc();
+
+        List<IndustryCategoryResponse> categoryResponses = categories.stream()
+                .map(category -> new IndustryCategoryResponse(
+                        category.getId(),
+                        category.getCategoryName()
+                ))
+                .toList();
+
+        return Map.of("content", categoryResponses);
     }
 }
--- src/main/java/com/sogonsogon/gonggomoonbackofficeapi/domain/industry/category/infrastructure/IndustryCategoryRepository.java
+++ src/main/java/com/sogonsogon/gonggomoonbackofficeapi/domain/industry/category/infrastructure/IndustryCategoryRepository.java
@@ -3,5 +3,10 @@ package com.sogonsogon.gonggomoonbackofficeapi.domain.industry.category.infrastr
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.category.entity.IndustryCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IndustryCategoryRepository extends JpaRepository<IndustryCategory, Long> {
+
+    List<IndustryCategory>findAllByOrderByCreatedAtAsc();
+
 }
