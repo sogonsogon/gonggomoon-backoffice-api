dc7f59244060908cd191668afd65a24c959ef2fe
JinYong833 <jjystudy1@gmail.com>
2026-03-08 16:02:09 +0900

    feat(IndustryAnalysis): 산업 분석 조회 (#7)
    - 산업 분석 목록 조회 추가
    - 산업 분석 단건 조회 추가

--- src/main/java/com/sogonsogon/gonggomoonbackofficeapi/domain/industry/analysis/application/IndustryAnalysisService.java
+++ src/main/java/com/sogonsogon/gonggomoonbackofficeapi/domain/industry/analysis/application/IndustryAnalysisService.java
@@ -2,10 +2,15 @@ package com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.applicat
 
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.dto.request.CreateReportRequest;
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.dto.request.UpdateReportRequest;
+import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.dto.response.ReportResponse;
+import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.dto.response.ReportsResponse;
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.entity.IndustryAnalysis;
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.infrastructure.IndustryAnalysisRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class IndustryAnalysisService {
@@ -19,6 +24,7 @@ public class IndustryAnalysisService {
     /**
      * 산업 분석 생성
      * 제약조건 고민 필요
+     * 카테고리 id가 없는 경우
      */
     @Transactional
     public void createReport(CreateReportRequest request, Long categoryId, Long userId) {
@@ -41,4 +47,37 @@ public class IndustryAnalysisService {
         industryAnalysis.update(request.categoryId(), request.analysisYear(), userId, request.competition(), request.marketSize(),
                 request.trend(), request.regulation(), request.keyword(), request.hiring(), request.investment());
     }
+
+    /**
+     * 해당 산업 카테고리 내 분석 목록 조회
+     */
+    @Transactional(readOnly = true)
+    public Map<String, List<ReportsResponse>> getReports(Long id) {
+
+        List<IndustryAnalysis> analyses = industryAnalysisRepository.findIndustryAnalysisByIndustryCategoryId(id);
+
+        List<ReportsResponse> responses = analyses.stream()
+                .map(analysis -> new ReportsResponse(
+                        analysis.getId(),
+                        analysis.getAnalysis_year(),
+                        analysis.getStatus(),
+                        analysis.getCreatedAt(),
+                        analysis.getUpdatedAt()
+                )).toList();
+
+        return Map.of("content", responses);
+    }
+
+    /**
+     * 분석 단건 조회
+     */
+    @Transactional(readOnly = true)
+    public ReportResponse getReport(Long id) {
+
+        IndustryAnalysis analysis = industryAnalysisRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+
+        return new ReportResponse(analysis.getAnalysis_year(), analysis.getKeyword(), analysis.getMarketSize(),
+                analysis.getTrend(), analysis.getRegulation(), analysis.getCompetition(), analysis.getHiring(), analysis.getInvestment());
+    }
+
 }
--- src/main/java/com/sogonsogon/gonggomoonbackofficeapi/domain/industry/analysis/presentation/IndustryAnalysisController.java
+++ src/main/java/com/sogonsogon/gonggomoonbackofficeapi/domain/industry/analysis/presentation/IndustryAnalysisController.java
@@ -3,16 +3,22 @@ package com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.presenta
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.application.IndustryAnalysisService;
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.dto.request.CreateReportRequest;
 import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.dto.request.UpdateReportRequest;
+import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.dto.response.ReportResponse;
+import com.sogonsogon.gonggomoonbackofficeapi.domain.industry.analysis.dto.response.ReportsResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/admin")
@@ -45,5 +51,17 @@ public class IndustryAnalysisController {
         industryAnalysisService.updateReport(request, reportId, Long.valueOf(details.getUsername()));
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/industries/{id}/reports")
+    public ResponseEntity<Map<String, List<ReportsResponse>>> getReports(@PathVariable Long id) {
+
+        return ResponseEntity.ok(industryAnalysisService.getReports(id));
+    }
+
+    @GetMapping("/industries/reports/{id}")
+    public ResponseEntity<ReportResponse> getReport(@PathVariable Long id) {
+
+        return ResponseEntity.ok(industryAnalysisService.getReport(id));
     }
 }
