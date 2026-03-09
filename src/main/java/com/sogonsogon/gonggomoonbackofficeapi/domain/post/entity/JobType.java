26c33f860923838ff55418588d82e28414afbf7e
JinYong833 <jjystudy1@gmail.com>
2026-03-09 19:20:13 +0900

    feat(PostSubmission): JobType
    - 나중에 테이블로 뺄 예정

---
+++ src/main/java/com/sogonsogon/gonggomoonbackofficeapi/domain/post/entity/JobType.java
@@ -0,0 +1,14 @@
+package com.sogonsogon.gonggomoonbackofficeapi.domain.post.entity;
+
+public enum JobType {
+    FRONTEND,
+    BACKEND,
+    EMBEDDED,
+    DEVOPS,
+    DATA_ANALYSIS,
+    AI,
+    INFORMATION_SECURITY,
+    DESIGN,
+    PM_PO,
+    QA
+}
